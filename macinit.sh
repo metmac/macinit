d24094ae762157317e6a6a65f6b1beed42857b20
Brandon Freitag <freitagbr@gmail.com>
2016-11-30 00:33:29 -0800

    Use LOG variable

--- macinit.sh
+++ macinit.sh
@@ -6,12 +6,12 @@
 # https://github.com/lapwinglabs/blog/blob/master/hacker-guide-to-setting-up-your-mac.md
 
 {
-  COUT="macinit.out"
+  LOG=macinit.log
 
   # install brew
   if test ! $(which brew); then
     echo "Installing homebrew..."
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" >> ${COUT}
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" >> $LOG
   fi
 
   # install binaries
@@ -49,10 +49,10 @@
     vim
     youtube-dl
   )
-  brew update >> ${COUT}
-  brew tap homebrew/dupes >> ${COUT}
-  brew install ${binaries[@]} >> ${COUT}
-  brew cleanup >> ${COUT}
+  brew update >> $LOG
+  brew tap homebrew/dupes >> $LOG
+  brew install ${binaries[@]} >> $LOG
+  brew cleanup >> $LOG
 
   # install apps
   echo "Installing apps..."
@@ -69,7 +69,7 @@
     transmission
     vlc
   )
-  brew cask install --appdir="/Applications" ${apps[@]} >> ${COUT}
+  brew cask install --appdir="/Applications" ${apps[@]} >> $LOG
 
   # setup git
   read -p 'Name: ' name
@@ -81,10 +81,10 @@
 
   # generate ssh key
   echo "Generating ssh key..."
-  ssh-keygen -t rsa -b 4096 -C "${email}" >> ${COUT}
+  ssh-keygen -t rsa -b 4096 -C "${email}" >> $LOG
   unset email
-  eval "$(ssh-agent -s)" >> ${COUT}
-  ssh-add ~/.ssh/id_rsa >> ${COUT}
+  eval "$(ssh-agent -s)" >> $LOG
+  ssh-add ~/.ssh/id_rsa >> $LOG
 
   # add key to github
   read -p 'GitHub username: ' un
@@ -92,36 +92,36 @@
   curl -u \
     "${un}:${pw}" \
     --data '{"title":"${HOSTNAME}","key":"$(cat ~/.ssh/id_rsa.pub)"}' \
-    https://api.github.com/user/keys >> ${COUT}
+    https://api.github.com/user/keys >> $LOG
   unset un
   unset pw
   echo "Added ssh key to GitHub."
 
   # clone repos
   read -p "Dotfiles repo path: " dotfiles
-  git clone https://github.com/${dotfiles}  ~/src/dotfiles >> ${COUT}
+  git clone https://github.com/${dotfiles}  ~/src/dotfiles >> $LOG
   unset dotfiles
   echo "Cloned dotfiles repo."
 
   # install dotfiles
   echo "Installing dotfiles..."
-  pushd ~/src/dotfiles >> ${COUT}
+  pushd ~/src/dotfiles >> $LOG
   ./install.sh
-  popd >> ${COUT}
+  popd >> $LOG
 
   # setup vim
   echo "Setting up vim..."
-  curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim >> ${COUT}
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim >> $LOG
   vim +PlugInstall +qall
   mkdir ~/.vim/undo
 
   # install nvm
   echo "Installing nvm..."
-  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash >> ${COUT}
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash >> $LOG
   source ~/.bash_profile
-  nvm install stable >> ${COUT}
-  nvm use stable >> ${COUT}
-  nvm alias default stable >> ${COUT}
+  nvm install stable >> $LOG
+  nvm use stable >> $LOG
+  nvm alias default stable >> $LOG
 
   # echo switch shells command
   echo "To finish setup, execute the following command:"
