36e17ad74d03f7255e134699cf3dda4dbec3eef1
Brandon Freitag <freitagbr@gmail.com>
2017-09-26 02:12:12 -0700

    Remove log file, remove brew cask and neovim

--- macinit.sh
+++ macinit.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 
 # macinit.sh
-# Brandon Freitag, 2016
+# Brandon Freitag, 2017
 
 {
-  LOG=macinit.log
-
   # install brew
   if test ! `which brew`; then
     echo "Installing homebrew..."
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" >> $LOG
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   fi
 
   # install binaries
@@ -18,8 +16,7 @@
     asciinema
     bash
     bash-completion
-    Caskroom/cask/brew-cask
-    Caskroom/cask/java
+    caskroom/cask/java
     cmake
     coreutils
     ffmpeg
@@ -27,13 +24,12 @@
     git
     gnutls
     golang
-    homebrew/dupes/grep
+    grep
     htop
     irssi
     leiningen
     lua
     nmap
-    nvim
     python
     python3
     rust
@@ -41,20 +37,10 @@
     vim
     youtube-dl
   )
-  brew update >> $LOG
-  brew tap homebrew/dupes >> $LOG
-  brew install ${binaries[@]} >> $LOG
-  brew cleanup >> $LOG
-
-  # install apps
-  echo "Installing apps..."
-  apps=(
-    chromium
-    iterm2
-    sketch
-    spotify
-  )
-  brew cask install --appdir="/Applications" ${apps[@]} >> $LOG
+  brew update
+  brew tap homebrew/dupes
+  brew install ${binaries[@]}
+  brew cleanup
 
   # setup git
   read -p 'Name: ' name
@@ -66,10 +52,10 @@
 
   # generate ssh key
   echo "Generating ssh key..."
-  ssh-keygen -t rsa -b 4096 -C "${email}" >> $LOG
+  ssh-keygen -t rsa -b 4096 -C "${email}"
   unset email
-  eval "$(ssh-agent -s)" >> $LOG
-  ssh-add ~/.ssh/id_rsa >> $LOG
+  eval "$(ssh-agent -s)"
+  ssh-add ~/.ssh/id_rsa
 
   # add key to github
   read -p 'GitHub username: ' un
@@ -77,14 +63,14 @@
   curl -u \
     "${un}:${pw}" \
     --data "{\"title\":\"${HOSTNAME}\",\"key\":\"$(cat ~/.ssh/id_rsa.pub)\"}" \
-    https://api.github.com/user/keys >> $LOG
+    https://api.github.com/user/keys
   unset un
   unset pw
   echo "Added ssh key to GitHub."
 
   # clone repos
   read -p "Dotfiles repo path: " dotfiles
-  git clone https://github.com/$dotfiles  ~/src/dotfiles >> $LOG
+  git clone https://github.com/$dotfiles  ~/src/dotfiles
   unset dotfiles
   echo "Cloned dotfiles repo."
 
@@ -96,17 +82,17 @@
 
   # setup vim
   echo "Setting up vim..."
-  curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim >> $LOG
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
   vim +PlugInstall +qall
   mkdir ~/.vim/undo
 
   # install nvm
   echo "Installing nvm..."
-  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash >> $LOG
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
   source ~/.bash_profile
-  nvm install stable >> $LOG
-  nvm use stable >> $LOG
-  nvm alias default stable >> $LOG
+  nvm install stable
+  nvm use stable
+  nvm alias default stable
 
   # echo switch shells command
   echo -e "To finish setup, execute the following command:\n"
